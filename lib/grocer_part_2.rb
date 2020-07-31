e49e27280e503c03f525679ea42f2a0c845f91f8
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-31 19:23:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -71,6 +71,7 @@ def checkout(cart, coupons)
   
   #now I will loop over the cart that has all the discounts
   total_price = 0.0
+  binding.pry
   cart_with_all_discounts.each do |cart_item|
     total += cart_item[:price] 
     binding.pry
